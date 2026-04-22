1d636054ef5a75c15f82c02f72ff672212331f0b
gromhorse90022110-dot <gromhorse90022110@gmail.com>
2026-04-22 19:28:50 +0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,19 @@
 use macroquad::prelude::*;
 
+#[no_mangle]
+pub extern "C" fn main_android() {}
+
 #[macroquad::main("my_app")]
 async fn main() {
     let mut color = BLUE;
-
     loop {
-        // Очищаем фон
         clear_background(BLACK);
-
-        // Если коснулись экрана — меняем цвет на случайный
         if is_mouse_button_pressed(MouseButton::Left) {
-            color = Color::new(
-                rand::gen_range(0.0, 1.0),
-                rand::gen_range(0.0, 1.0),
-                rand::gen_range(0.0, 1.0),
-                1.0,
-            );
+            color = Color::new(rand::gen_range(0.0, 1.0), rand::gen_range(0.0, 1.0), rand::gen_range(0.0, 1.0), 1.0);
         }
-
-        // Рисуем круг в центре
         draw_circle(screen_width() / 2.0, screen_height() / 2.0, 100.0, color);
-
-        // Рисуем текст-инструкцию
         draw_text("TAP TO CHANGE COLOR", 20.0, 50.0, 40.0, WHITE);
         draw_text("RUST VERSION 2.0", 20.0, 100.0, 30.0, GREEN);
-
         next_frame().await
     }
 }
