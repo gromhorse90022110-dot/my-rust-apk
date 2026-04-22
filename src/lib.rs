c03caa201ed1a5e50860ecca184e6b9ba1e6c766
gromhorse90022110-dot <gromhorse90022110@gmail.com>
2026-04-22 20:00:24 +0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,16 @@
 use macroquad::prelude::*;
 
-// Эта магия связывает Rust и Android, чтобы не было вылета
-#[cfg(target_os = "android")]
+// Упрощенная "магия" для запуска на Android
 #[no_mangle]
-fn android_main(app: ndk_glue::android_app::AndroidApp) {
-    // Пустая функция для инициализации
-}
+pub extern "C" fn ANativeActivity_onCreate() {}
 
 #[macroquad::main("my_app")]
 async fn main() {
     let mut color = BLUE;
+
     loop {
         clear_background(BLACK);
-        
+
         if is_mouse_button_pressed(MouseButton::Left) {
             color = Color::new(
                 rand::gen_range(0.0, 1.0),
@@ -24,8 +22,8 @@ async fn main() {
 
         draw_circle(screen_width() / 2.0, screen_height() / 2.0, 100.0, color);
         draw_text("TAP TO CHANGE COLOR", 20.0, 50.0, 40.0, WHITE);
-        draw_text("RUST VERSION 2.0", 20.0, 100.0, 30.0, GREEN);
-        
+        draw_text("VERSION 2.1", 20.0, 100.0, 30.0, GREEN);
+
         next_frame().await
     }
 }
